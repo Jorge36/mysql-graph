5d7371812f22d259034d7041e3f144e70dc671ab
Jorge36 <36677503+Jorge36@users.noreply.github.com>
2019-06-10 11:45:55 +0100

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,12 +1,12 @@
-WITH RECURSIVE blue(id, camino) AS (
+WITH RECURSIVE blue(id, paths) AS (
 		SELECT id, CAST(id AS CHAR(200)) FROM nodes WHERE color = "yellow"
 		UNION ALL
-		SELECT e.node_to, concat(b.camino, ',', e.node_to)
+		SELECT e.node_to, concat(b.paths, ',', e.node_to)
 		FROM blue b INNER JOIN edges e
 		  ON e.node_from = b.id
-		WHERE find_in_set(e.node_to, b.camino)=0
+		WHERE find_in_set(e.node_to, b.paths)=0
 )
-SELECT SUBSTRING(b.camino, 1, 1) yellow, b.id blue FROM blue b INNER JOIN nodes n
+SELECT SUBSTRING(b.paths, 1, 1) yellow, b.id blue FROM blue b INNER JOIN nodes n
 ON b.id = n.id 
 WHERE n.color = "blue" 
 GROUP BY yellow, blue
@@ -14,15 +14,15 @@ ORDER BY yellow, blue;
 
 -- Or
 
-WITH RECURSIVE blue(id, camino) AS (
+WITH RECURSIVE blue(id, paths) AS (
 		SELECT id, CAST(id AS CHAR(200)) FROM nodes WHERE color = "yellow"
 		UNION ALL
-		SELECT e.node_to, concat(b.camino, ',', e.node_to)
+		SELECT e.node_to, concat(b.paths, ',', e.node_to)
 		FROM blue b INNER JOIN edges e
 		  ON e.node_from = b.id
-		WHERE find_in_set(e.node_to, b.camino)=0
+		WHERE find_in_set(e.node_to, b.paths)=0
 )
-SELECT SUBSTRING(b.camino, 1, 1) yellow, b.id blue, COUNT(SUBSTRING(b.camino, 1, 1)) paths FROM blue b INNER JOIN nodes n
+SELECT SUBSTRING(b.paths, 1, 1) yellow, b.id blue, COUNT(SUBSTRING(b.paths, 1, 1)) "Number of paths" FROM blue b INNER JOIN nodes n
 ON b.id = n.id 
 WHERE n.color = "blue" 
 GROUP BY yellow, blue
